0448d6ffde4b19ef4ccfba417721863b35f2aed2
yaozaiwind <yaozaiwudi@163.com>
2018-03-15 10:58:52 +0800

    终于能发钱了

--- bbx-interface.py
+++ bbx-interface.py
@@ -5,13 +5,14 @@ import requests,hashlib,time,json
 
 class bbxrequest:
 
-    access_key1 = "a10000001"
-    secret1 = "b10000001"
+    access_key1 = "a10000001"   #账号1 的 登陆密钥
+    secret1 = "b10000001"       #账号1的 加密密钥
     access_key2 = "a10000002"
     secret2 = "b10000002"
+    # header
     header = {
     "Host": "api.bbx.com",
-    "Bbx-Accesskey" : "a10000001",
+    "Bbx-Accesskey" : "a10000001",  #此处是登录密钥
     "Bbx-Sign": "",
     "Accept": "*/*",
     "Connection": "keep-alive",
@@ -20,6 +21,8 @@ class bbxrequest:
     "User-Agent": "BTStore/1.0 (iPhone; iOS 11.2.2; Scale/2.00)",
     "Content-Type": "application/json"
     }
+
+    #  Way :1 和2 分别是买卖单类型  
     submint_oder_buy_data = {"category": 1, "fee_coin_code": "BBX", "stock_code": "ETH/BBX", "price": 2000000000, "vol": 100000000,"way": 1, "nonce": 1520315223}
 
     def __init__(self):
@@ -34,7 +37,7 @@ class bbxrequest:
         print("BODY is : ",json.dumps(self.submint_oder_buy_data))
         self.request()
     def getorder(self):
-        pass
+        pass  #这段还没通
         orderrequest = requests.get('http://api.bbx.com/v1/ifmarket/vipGetOrders')
 
     def ordercancel(self):
