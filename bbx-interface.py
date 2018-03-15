307b55753a28bc7be09f84e199cd2f36c3033361
yaozaiwind <yaozaiwudi@163.com>
2018-03-15 11:43:19 +0800

    终于能发钱了

--- bbx-interface.py
+++ bbx-interface.py
@@ -22,7 +22,7 @@ class bbxrequest:
     "Content-Type": "application/json"
     }
 
-    #  Way :1 和2 分别是买卖单类型  
+    #  Way :1 和2 分别是买卖单类型
     submint_oder_buy_data = {"category": 1, "fee_coin_code": "BBX", "stock_code": "ETH/BBX", "price": 2000000000, "vol": 100000000,"way": 1, "nonce": 1520315223}
 
     def __init__(self):
