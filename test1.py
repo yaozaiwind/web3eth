238d350ed80a6a628769192da16c5670e91bd240
Joe Rang <yaozaiwudi@163.com>
2018-03-24 20:38:25 +0800

    1

--- test1.py
+++ test1.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-browser = webdriver.Chrome()
-time.sleep(1)
-browser.get('http://www.qmzs.com/categroy')
-assert "全民助手" in browser.title
-ele = browser.find_element_by_name("searchInput")
-ele.clear()
-ele.send_keys("部落冲突")
-ele.send_keys(Keys.ENTER)
-print("开始搜")
-time.sleep(1)
-btn = browser.find_element_by_id("ss_searchBtn")
-btn.click()
-time.sleep(1)
-#browser.close()
+
+def test():
+    browser = webdriver.Chrome()
+    time.sleep(1)
+    browser.get('http://www.qmzs.com/categroy')
+    assert "全民助手" in browser.title
+    ele = browser.find_element_by_name("searchInput")
+    ele.clear()
+    ele.send_keys("部落冲突")
+    ele.send_keys(Keys.ENTER)
+    print("开始搜")
+    time.sleep(1)
+    btn = browser.find_element_by_id("ss_searchBtn")
+    btn.click()
+    time.sleep(1)
+    #browser.close()
+
+def test1():
+    browser = webdriver.ChromeOptions()
+
+if __name__ == '__main__':
+    pass
